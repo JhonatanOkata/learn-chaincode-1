5a820781c10d34df21b07761ce3a65a247fcd769
JhonatanOkata <JhonatanOkata@yahoo.com.br>
2016-12-14 15:04:04 -0200

    Update chaincode_finished.go
    agora sim

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -70,8 +70,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		var msg = "Teste Jhonatan Okata" + args[0]	
-		return []byte(msg), nil 		
-		//return t.read(stub, args)
+		//return []byte(msg), nil 		
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
